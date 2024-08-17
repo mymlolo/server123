65657e3b8ecf443c4082e7ade26d6681f87bb6d1
mymlolo <c.nahuelprado65@gmail.com>
2024-08-17 01:01:26 +0000

    Mundo guardado 2024-08-17

--- server.py
+++ server.py
@@ -3,7 +3,8 @@
 
 # Puedes instalar mohist después de instalar forge desde el menú de gestionar
 # Puedes instalar paper después de instalar vanilla desde el menú de gestionar
-# Puedes instalar purpur después de instalar fabric desde el menú de gestonar
+# Puedes instalar purpur después de instalar fabric desde el menú de gestionar
+
 
 
 # Regiones de ngrok
